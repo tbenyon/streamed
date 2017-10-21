9a53a0075e9884a312fea7d00dfddf657233c6b7
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-10-21 08:31:56 +0100

    Save a new user without validation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pickle
 users = [
     {
         "name": "Tom",
+        "password": "pw1",
         "address": "123 Monkey Road",
         "dob": "15/1/1987",
         "gender": "male",
@@ -10,6 +11,7 @@ users = [
      },
     {
         "name": "Gerald",
+        "password": "pw2",
         "address": "234 Elephant Road",
         "dob": "15/1/1986",
         "gender": "male",
@@ -27,8 +29,30 @@ def load_users():
     pickle_file = open('data/users.pickle', 'rb')
     users = pickle.load(pickle_file)
     pickle_file.close()
-    print(users)
 
-save_users()
+def create_user():
+    requested_user_name = input("What would you like your username to be?")
+    password = input("What would you like your password to be?")
+    dob = input("What is your dob? (DD/MM/YYYY)")
+    gender = input("What is your gender?")
+    address = input("What is your address?")
+
+    user = {
+        "name": requested_user_name,
+        "password": password,
+        "address": address,
+        "dob": dob,
+        "gender": gender,
+    }
+
+    users.append(user)
 
 load_users()
+
+print(users)
+
+create_user()
+
+print(users)
+
+save_users()
